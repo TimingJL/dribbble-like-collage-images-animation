c736e1894b3b54dfffe13eb5fd782730f7f16512
Taiming Chen <taiming@bridgewell.com>
2019-06-10 22:19:16 +0800

    add header

---
+++ src/containers/CollageImages/Styled.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const StyledHeader = styled.h1`
+  font-size: 30vw;
+  color: #333;
+  opacity: 0.1;
+  transition: all 1s ease-in-out;
+`;
---
+++ src/containers/CollageImages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {
+  StyledHeader,
+} from './Styled';
+
+const CollageImages = () => (
+  <section>
+    <StyledHeader>404</StyledHeader>
+  </section>
+);
+
+export default CollageImages;
