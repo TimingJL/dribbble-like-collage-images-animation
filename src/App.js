ec3ea9443c55aab19529a0e965cd3a59c70f69a8
Taiming Chen <taiming@bridgewell.com>
2019-06-22 22:51:20 +0800

    [feature] add github corner

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 // import logo from './logo.svg';
 // import './App.css';
+import GithubCorner from 'react-github-corner';
 import CollageImages from 'containers/CollageImages';
 
 function App() {
   return (
     <main>
       <CollageImages />
+      <GithubCorner href="https://github.com/TimingJL/dribbble-like-collage-images-animation" />
     </main>
   );
 }
